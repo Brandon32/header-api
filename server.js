1d505b42557632baa72eceeb11950a58e5f6f0a8
Brandon <kem5brandon@gmail.com>
2016-12-01 20:16:28 +0000

    Add inital working app

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    console.log(req.headers);
-    res.send(JSON.stringify(req.headers));
+    var ip = req.get('X-Forwarded-For').split(', ');
+    var lang = req.get('accept-language').split(',');
+    var soft = req.get('user-agent').split(/\s?[\(\)]\s?/);
+    var whoami = {
+        "ipaddress":ip[0],
+        "language":lang[0],
+        "software":soft[1]
+    };
+    console.log(ip,lang,soft);
+    res.send(JSON.stringify(whoami));
 });
 
 app.listen(8080, function () {
