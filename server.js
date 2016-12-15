3578f5ebd3700063abb74cd37d76f498737d5d70
Brandon <kem5brandon@gmail.com>
2016-12-15 22:07:52 +0000

    Add Heroku

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
 
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
-    var ip = req.get('X-Forwarded-For').split(', ');
-    var lang = req.get('accept-language').split(',');
-    var soft = req.get('user-agent').split(/\s?[\(\)]\s?/);
-    var whoami = {
-        "ipaddress":ip[0],
-        "language":lang[0],
-        "software":soft[1]
-    };
-    console.log(ip,lang,soft);
-    res.send(JSON.stringify(whoami));
+  var ip = req.get('X-Forwarded-For').split(', ');
+  var lang = req.get('accept-language').split(',');
+  var soft = req.get('user-agent').split(/\s?[\(\)]\s?/);
+  var whoami = {
+    "ipaddress":ip[0],
+    "language":lang[0],
+    "software":soft[1]
+  };
+  console.log(ip,lang,soft);
+  res.send(JSON.stringify(whoami));
 });
 
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log('Header Parser app listening on port 8080!');
 });
